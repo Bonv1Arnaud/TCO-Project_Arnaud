4f40297ef3156af57545424ca28e1bc038a95ef0
Elise zanou <elise.zanou@students.hevs.ch>
2023-03-07 10:32:12 +0100

    Data PointListener + DataConnector = commentaires

--- src/main/java/ch/hevs/isi/core/DataPointListener.java
+++ src/main/java/ch/hevs/isi/core/DataPointListener.java
@@ -2,6 +2,6 @@ package ch.hevs.isi.core;
 
 public interface DataPointListener
 {
-//
+// methode
     public void onNewValue(DataPoint dp);
 }
--- src/main/java/ch/hevs/isi/db/DatabaseConnector.java
+++ src/main/java/ch/hevs/isi/db/DatabaseConnector.java
@@ -5,25 +5,24 @@ import ch.hevs.isi.core.DataPointListener;
 
 public class DatabaseConnector implements DataPointListener {
 
-    // instance database
+    // implementation variables
     private static DatabaseConnector instance = null;
 
-
+    // constructor
     private DatabaseConnector() {
-
     }
 
-    // methode
+    // implementation methode
+    // create a new instance if none existed
     public DatabaseConnector getInstance() {
         if (instance == null) {
             instance = new DatabaseConnector();
         }
         return instance;
-
     }
-
+    // print information of the value and data point
     @Override
     public void onNewValue(DataPoint dp) {
-        System.out.println("the new value"+ dp.getValue() +"the data point " + dp.getLabel() );
+        System.out.println("the new value" + dp.getValue() + "the data point " + dp.getLabel());
     }
 }
