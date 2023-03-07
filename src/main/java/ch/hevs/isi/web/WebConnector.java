b8f638fc51fdca5641c9d6560c426defa7ea8a5e
Bonv1Arnaud <arnaudbo01@hotmail.com>
2023-03-07 10:22:56 +0100

    DW-dataBaseConnector

--- src/main/java/ch/hevs/isi/web/WebConnector.java
+++ src/main/java/ch/hevs/isi/web/WebConnector.java
@@ -3,11 +3,11 @@ package ch.hevs.isi.web;
 public class WebConnector {
     WebConnector wc = new WebConnector();
 
-    public static WebConnector getInstance(){
+ /*   public static WebConnector getInstance(){
 
         return wc;
     }
-
+*/
     private void pushToWeb (String _label, String _value){
 
     }
