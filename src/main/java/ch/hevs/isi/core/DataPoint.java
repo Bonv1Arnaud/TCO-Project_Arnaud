c89718e1a1769a559521be01dfb385d575ad21ba
Elise zanou <elise.zanou@students.hevs.ch>
2023-03-07 09:24:25 +0100

    DataPoint implementation

--- src/main/java/ch/hevs/isi/core/DataPoint.java
+++ src/main/java/ch/hevs/isi/core/DataPoint.java
@@ -1,4 +1,36 @@
 package ch.hevs.isi.core;
 
+import com.sun.javafx.binding.StringFormatter;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataPoint {
+    // implementation variables
+    private String label;
+    private boolean isOutput;
+
+    // implementation method
+    // method dataPointMap
+    private static Map<String, DataPoint> dataPointMap = new HashMap<>();
+
+    // methode getLabel
+    public String getLabel(String label) {
+        this.label = label;
+        return label;
+    }
+
+    // method isOutput
+    public boolean isOutput(boolean isOutput) {
+        this.isOutput = isOutput;
+        return isOutput;
+    }
+
+    // implementation builder
+    DataPoint(String label, boolean isOutput) {
+        this.label = label;
+        this.isOutput = isOutput;
+        dataPointMap.put(label, this);
+    }
+
 }
