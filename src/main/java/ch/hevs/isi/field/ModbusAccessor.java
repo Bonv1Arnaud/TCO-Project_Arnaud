9625ed235b230631c9c42aad97ce6a8dbd1a87e8
Elise zanou <elise.zanou@students.hevs.ch>
2023-03-10 19:06:02 +0100

    ModbusAccessor class implementation

--- src/main/java/ch/hevs/isi/field/ModbusAccessor.java
+++ src/main/java/ch/hevs/isi/field/ModbusAccessor.java
@@ -1,4 +1,38 @@
 package ch.hevs.isi.field;
 
 public class ModbusAccessor {
+    private int regAddress;
+    private float newValue;
+    private boolean _newValue;
+     private static ModbusAccessor  instance = null;
+
+    private ModbusAccessor() {
+    }
+
+public static ModbusAccessor getInstance()
+    {if( instance == null)
+    {
+       instance = new ModbusAccessor(); 
+    }
+    return instance; }
+
+    public void readFloat(int regAddress) {
+        this.regAddress = regAddress;
+    }
+
+    public void writeFloat(int regAddress, float newValue) {
+        this.regAddress = regAddress;
+        this.newValue = newValue;
+    }
+
+    public void readBoolean(int regAddress) {
+        this.regAddress = regAddress;
+    }
+
+    public void writeBoolean(int regAddress, boolean _newValue) {
+        this.regAddress = regAddress;
+        this._newValue = _newValue;
+    }
+
+
 }
