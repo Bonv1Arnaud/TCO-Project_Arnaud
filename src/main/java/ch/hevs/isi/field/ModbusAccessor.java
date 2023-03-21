d3b2117f3b937491032b10d7f1d9f75030e2032d
Elise zanou <elise.zanou@students.hevs.ch>
2023-03-21 09:30:08 +0100

    connect

--- src/main/java/ch/hevs/isi/field/ModbusAccessor.java
+++ src/main/java/ch/hevs/isi/field/ModbusAccessor.java
@@ -1,20 +1,25 @@
 package ch.hevs.isi.field;
 
+import com.serotonin.modbus4j.ModbusFactory;
+import com.serotonin.modbus4j.ModbusMaster;
+import com.serotonin.modbus4j.exception.ModbusInitException;
+import com.serotonin.modbus4j.ip.IpParameters;
+
 public class ModbusAccessor {
     private int regAddress;
     private float newValue;
     private boolean _newValue;
-     private static ModbusAccessor instance = null;
+    private static ModbusAccessor instance = null;
 
     private ModbusAccessor() {
     }
 
-public static ModbusAccessor getInstance()
-    {if( instance == null)
-    {
-       instance = new ModbusAccessor();
+    public static ModbusAccessor getInstance() {
+        if (instance == null) {
+            instance = new ModbusAccessor();
+        }
+        return instance;
     }
-    return instance; }
 
     public void readFloat(int regAddress) {
         this.regAddress = regAddress;
@@ -35,4 +40,25 @@ public static ModbusAccessor getInstance()
     }
 
 
+
+    public void connect(String ipAddress, int port) {
+        ModbusFactory modbusFactory = new ModbusFactory();
+        IpParameters ip = new IpParameters();
+        ip.setHost(ipAddress);
+        ip.setPort(port);
+        ModbusMaster tcpMaster = modbusFactory.createTcpMaster(ip, true);
+        while (true) {
+            try {
+                tcpMaster.init();
+                break;
+            } catch (ModbusInitException e) {
+                System.out.println("Unable to connect to the server");
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            continue;
+        }
+    }
 }
