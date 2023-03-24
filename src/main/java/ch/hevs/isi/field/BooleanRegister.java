472d8c0e9ab676e966e0dbd8d8994f55517657a1
Bonv1Arnaud <arnaudbo01@hotmail.com>
2023-03-24 13:22:12 +0100

    creation class boolean et float register avec methode vide

--- src/main/java/ch/hevs/isi/field/BooleanRegister.java
+++ src/main/java/ch/hevs/isi/field/BooleanRegister.java
@@ -1,4 +1,31 @@
 package ch.hevs.isi.field;
 
+import ch.hevs.isi.core.BooleanDataPoint;
+import ch.hevs.isi.core.FloatDataPoint;
+
 public class BooleanRegister {
+    private int adress;
+    private BooleanDataPoint dp;
+
+    public BooleanRegister(int _adress, BooleanDataPoint _dp){
+        adress = _adress;
+        dp = _dp;
+    }
+
+    public void read(){
+        boolean value = ModbusAccessor.getInstance().readBoolean(adress);   // Take the value on the modbus
+        dp.setValue(value);                                                 // Put the value in the datapoint
+    }
+
+    public void write(){
+        boolean value = (boolean) dp.getValue();                            // Take the value in the datapoint
+        ModbusAccessor.getInstance().writeBoolean(adress,value);            // Put the value on the modbus
+    }
+
+    public BooleanRegister getRegisterFromDataPoint(BooleanDataPoint bdp){
+        BooleanRegister br = null;
+
+        return br;
+    }
+
 }
