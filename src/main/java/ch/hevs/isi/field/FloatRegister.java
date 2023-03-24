cec4d9c8bd5746973b68565fc7f6475202fdb1bf
Bonv1Arnaud <arnaudbo01@hotmail.com>
2023-03-24 14:12:02 +0100

    implementation methode float et boolean register + creation ModbusRegister

--- src/main/java/ch/hevs/isi/field/FloatRegister.java
+++ src/main/java/ch/hevs/isi/field/FloatRegister.java
@@ -6,29 +6,26 @@ import ch.hevs.isi.core.FloatDataPoint;
 public class FloatRegister {
 
     private int adress;
+    private static ModbusAccessor ma;
     private FloatDataPoint dp;
 
-    public FloatRegister(int _adress, FloatDataPoint _dp){
+    public FloatRegister(int _adress, FloatDataPoint _dp) {
         adress = _adress;
         dp = _dp;
     }
 
-    public void read(){
+    public static void setModbusAccessor (ModbusAccessor accessor){
+        ma = accessor;
+    }
+
+    public void read() {
         float value = ModbusAccessor.getInstance().readFloat(adress);       // Take the value on the modbus
         dp.setValue(value);                                                 // Put the value in the datapoint
     }
 
-    public void write(){
+    public void write() {
         float value = (float) dp.getValue();                                // Take the value in the datapoint
-        ModbusAccessor.getInstance().writeFloat(adress,value);              // Put the value on the modbus
+        ModbusAccessor.getInstance().writeFloat(adress, value);              // Put the value on the modbus
     }
 
-    public FloatRegister getRegisterFromDataPoint(FloatDataPoint fdp){
-        FloatRegister fr = null;
-
-        return fr;
-    }
-
-
-    //methode getRegisterFromDataPoint
 }
